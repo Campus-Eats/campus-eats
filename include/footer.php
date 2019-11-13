49eae74910390bb57af8d516a868084170c322bb
Eileen Santiago <eileenmvs@gmail.com>
2019-11-12 19:15:08 -0500

    updated home (to php) and footer

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,42 @@
+<?php
+    // footer.php
+    // To be included in all pages
+?>
 <footer>
-    <p>This website uses cookies to optimize your experience.
-    <a href="#" target="_blank">Click here to accept cookies</a>.</p>
+    <div class="company">
+                <h4>Company</h4>
+                <ul>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Terms of Use</a></li>
+                    <li><a href="#">Private Policy</a></li>
+                </ul>
+
+            </div>
+            <div class="product">
+                <h4>Product</h4>
+                <ul>
+                    <li><a href="#">Partnerships</a></li>
+                    <li><a href="#">Sponsors</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="connect">
+                <h4>Connect</h4>
+                <ul>
+                    <div class=socialmedia>
+
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"> <i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+
+                    </div>
+                    <li><a href="#">Support@campuseats.ca</a></li>
+                    <li><a href="#">+123 456 7890</a></li>
+                </ul>
+
+            </div>
+        </footer>
+        <p>This website uses cookies to optimize your experience.
+        <a href="#" target="_blank">Click here to accept cookies</a>.</p>
 </footer>
