3cf6f2d0690e90648d8c9c4190b6c7dbdfc3ee2b
Eileen Santiago <eileenmvs@gmail.com>
2019-12-05 13:57:50 -0500

    added image path

--- processing-program-register.php
+++ processing-program-register.php
@@ -8,15 +8,27 @@ $streetAddress = $_POST['streetAddress'];
 $city = $_POST['city'];
 $phone = $_POST['phone'];
 $hours = $_POST['hours'];
+$filename = $_FILES['image']['name'];
 
 include("include/connect.php");
 
-$stmt = $pdo->prepare("INSERT INTO `foodprograms` (`programID`, `programName`, `email`, `password`,`streetAddress`, `city`, `phone`, `hours`) VALUES (NULL, '$programName', '$email', '$password', :streetAddress, '$city', :phone, :hours)");
+$uploadF = '../images/';
+$uploadfile = $uploadF . basename($filename);
+
+if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible image file missing!\n";
+};
+
+$fullImagePath = "/campus-eats/images/" . $filename;
+
+
+$stmt = $pdo->prepare("INSERT INTO 
+    `foodprograms` (`programID`, `programName`, `email`, `password`,`streetAddress`, `city`, `phone`, `hours`, `image`) 
+    VALUES (NULL, '$programName', '$email', '$password', :streetAddress, '$city', :phone, :hours, '$fullImagePath')
+    ");
 
 $stmt->execute(array(':hours' => $hours, ':phone' => $phone, ':streetAddress' => $streetAddress));
-
-
-
-
 
 ?>
