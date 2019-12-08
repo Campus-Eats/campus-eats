c8ab3b55e08cd719acaec609691de767768ebfae
yangzm018 <46513489+yangzm018@users.noreply.github.com>
2019-12-07 21:46:38 -0500

    Update processing-program-register.php

--- processing-program-register.php
+++ processing-program-register.php
@@ -1,29 +1,44 @@
-<?php
-session_start();
-
-$programName = $_POST['programName'];
-$description = $_POST['description'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$address = $_POST['address'];
-$addressDetails = $_POST['addressDetails'];
-$city = $_POST['city'];
-$phone = $_POST['phone'];
-$hours = $_POST['hours'];
-// $uploaddir = 'assets/';
-// $image = $uploaddir.basename($_FILES['image']['name']);
-
-// echo($image);
-
+<?php session_start();
 include("include/connect.php");
 
-$stmt = $pdo->prepare("INSERT INTO `foodprograms` ( `programName`, `description`, `email`, `password`, `address`, `addressDetails`, `city`, `phone`, `hours`) VALUES ('$programName', :description, '$email', '$password', :address, :addressDetails, '$city', :phone, :hours)");
+// error_reporting(E_ALL ^ E_NOTICE);
 
-$stmt->execute(array(':description' => $description, ':address' => $address, ':addressDetails' => $addressDetails, ':phone' => $phone, ':hours' => $hours));
+$programName = $_POST["programName"];
+$description = $_POST["description"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$address = $_POST["address"];
+$addressDetails = $_POST["addressDetails"];
+$city = $_POST["city"];
+$phone = $_POST["phone"];
+$hours = $_POST["hours"];
 
-// echo('{"success":"true"}');
+$uploaddir = 'assets/';
+$image = $uploaddir . basename($_FILES['image']['name']);
 
-var_dump($stmt);
-// header("Location:login.php");
-​
+
+
+$stmt = $pdo->prepare("INSERT INTO `foodprograms`(`programName`,`description`,`email`,`password`,`address`,`addressDetails`,`city`,`phone`,`hours`,`image`) VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? );");
+$stmt->execute([$programName,$description,$email,$password,$address,$addressDetails,$city,$phone,$hours,$image]);
+
+// var_dump($_POST);
+
+// var_dump($stmt->errorInfo());
+
+
+
+if (move_uploaded_file($_FILES['image']['tmp_name'], $image)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "File upload failed.\n";
+}
+
+// echo 'Here is some more debugging info:';
+// print_r($_FILES);
+
+
+header("Location:login.php");
+
 ?>
+
+
