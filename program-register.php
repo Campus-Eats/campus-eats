6574054463234e7683af857bdfa9b8e070c016f4
yangzm018 <46513489+yangzm018@users.noreply.github.com>
2019-12-07 21:47:25 -0500

    Update program-register.php

--- program-register.php
+++ program-register.php
@@ -25,20 +25,37 @@ session_start();
 
                 <div id="programRegister-item2">
                     <h1 id="welcome">Program Register</h1>
-                    <form class="text" action="processing-program-register.php" method="POST">
+                    <form class="text" action="processing-program-register.php" method="POST" enctype="multipart/form-data">
                         <p class="textProgramRegister">Program Name</p><input class="textfield" name="programName" type="text" required />
+
                         <p class="textProgramRegister">Email</p><input class="textfield" name="email" type="email" required />
+
                         <p class="textProgramRegister">Password</p><input class="textfield" name="password" type="password" required />
 
-                        <p class="textProgramRegister">Street Address</p><input class="textfield" name="streetAddress" type="text" required />
-                        <p class="textProgramRegister">City</p><input class="textfield" name="city" type="text" required />
-                        <p class="textProgramRegister">Phone Number</p><input class="textfield" name="phone" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx" required />
-                        <p class="textProgramRegister">Hours</p><input class="textfield" name="hours" type="text" required />
+                        <p class="textProgramRegister">Street Address</p><input class="textfield" name="address" type="text" required />
 
+                        <p class="textProgramRegister">Location on Campus</p><input class="textfield" name="addressDetails" type="text" required />
+
+                        <p class="textProgramRegister">City</p><input class="textfield" name="city" type="text" required />
+
+                        <p class="textProgramRegister">Phone Number</p><input class="textfield" name="phone" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx" required />
+
+                        <p class="textProgramRegister">Operation Hours</p><input class="textfield" name="hours" type="text" required />
+                        <br>
+                        <p class="textProgramRegister">Description About Your Program</p>
+                        <textarea rows="2" cols="200" style="height:100px !important;"class="textfield" placeholder="Write few lines about your program......" name="description"></textarea>
+                        <br>
+
+                        <p class="textProgramRegister">Upload Image</p>
+                        <input type="hidden" name="size" value="100000"/>
+                        <div>
+                            <input type="file" name="image">
+                        </div>
 
                         <p id="conditions">By continuing the registration process, I agree to the <a id="terms" href="terms.php">Terms and Conditions.</a></p>
+                        
 
-                        <input id="button" type="submit">
+                        <input id="button" type="submit" name="upload" value="Register" >
 
                     </form>
                 </div>
