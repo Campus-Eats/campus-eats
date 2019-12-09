197efe380b6afcaf25a6c14eca26e501afed4e04
AlenaK06 <54857769+AlenaK06@users.noreply.github.com>
2019-12-08 21:24:24 -0500

    logout.php and logout showing in nav when logged in

--- include/header.php
+++ include/header.php
@@ -8,17 +8,45 @@
                 <!-- <li><a href="search.php">Search</a></li> -->
                 <li><a href="contact.php">Contact</a></li>
                 <li><a href="register.php">Register</a></li>
-                <li><a href="login.php">LogIn</a></li>
+
+                <?php 
+                if(! isset($_SESSION['userID']) || ! isset($_SESSION['programID'])) { ?>
+                    <li><a href="/campus-eats/login.php">LogIn</a></li>
+                <?php
+                };
+
+                if(isset($_SESSION['userID']) || isset($_SESSION['programID'])){
+                ?>
+                    <li><a href="/campus-eats/logout.php">LogOut</a></li>
+                <?php 
+                };
+                ?>
             </ul>
+
+
             <div class="hamburger-menu">
                 <label for="toggle" onclick="showHam()">&#9776;</label>
                 <section id="hamMenu">
-                    <li><a href="home.php">Home</a></li>
-                    <li><a href="about.php">About</a></li>
-                    <!-- <li><a href="search.php">Search</a></li> -->
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="register.php">Register</a></li>
-                    <li><a href="login.php">LogIn</a></li> 
+                    <ul>
+                        <li><a href="home.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <!-- <li><a href="search.php">Search</a></li> -->
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="register.php">Register</a></li>
+                        <?php 
+                        if(! isset($_SESSION['userID']) || ! isset($_SESSION['programID'])) { ?>
+                            <li><a href="/campus-eats/login.php">LogIn</a></li>
+                        <?php
+                        };
+
+                        if(isset($_SESSION['userID']) || isset($_SESSION['programID'])){
+                        ?>
+                            <li><a href="/campus-eats/logout.php">LogOut</a></li>
+                        <?php 
+                        };
+                        ?>
+
+                    </ul>
                 </section>
                 <!-- <input type="checkbox" id="toggle" /> -->
             </div>
---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php 
+session_start();
+session_destroy();
+
+header("Location:/campus-eats/home.php");
+?>
+
+<!-- <p>You have logged out.</p>
+<a href="login.php">Login again</a>
+<a href="home.php">Go back home</a> -->
+
+<?php
+// include("include/footer.php");
+?>
