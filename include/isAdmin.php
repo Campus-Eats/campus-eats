b17a7ffff2668aceac214e08e25bfb82b614d06c
Eileen Santiago <eileenmvs@gmail.com>
2019-12-08 20:48:38 -0500

    added helper includes isAdmin and isLogged

--- include/isAdmin.php
+++ include/isAdmin.php
@@ -1,17 +1,11 @@
 <?php
-    // checkes to see if program user (Admin) is logged in/ in session
     if(!isset($_SESSION)) {
         session_start();
     }
-    // $isAdmin is initialized as false 
     $isAdmin = false;
-    if(isset($_SESSION['programID'])) {
-
-        $user_program_admin = $_SESSION['programID'];
-        
-        include("connect.php");
-        $_stmt = $pdo->prepare("SELECT * FROM `foodprograms` WHERE `programID`= $user_program_admin");    
-        $_stmt->execute();
-        $_row= $_stmt->fetch();
+    if(isset($_SESSION)) {
+        if(isset($_SESSION['isAdmin'])){ 
+            $isAdmin = true;
+        }
     }
 ?>
