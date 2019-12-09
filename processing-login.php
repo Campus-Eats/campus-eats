7b017eeaf9f52688effc322eebc0039aee6c6eac
AlenaK06 <54857769+AlenaK06@users.noreply.github.com>
2019-12-08 22:14:06 -0500

    log in as foodprogram SESSION stores

--- processing-login.php
+++ processing-login.php
@@ -13,9 +13,23 @@ $stmt->execute();
 $row = $stmt->fetch();
 
 if ($row['email']==$email && $row['password']==$password){
-    header("Location:home.php");
+    $_SESSION['userID'] = $row['userID'];
+
 }else{
     echo("Please try again.");
+
+    $stmt = $pdo->prepare("SELECT * FROM `foodprograms` WHERE `email` = '$email' AND `password` = '$password' ");
+
+    $stmt->execute();
+
+    $row= $stmt->fetch();
+
+    if($row['email']==$email && $row['password']==$password){
+        header("Location:home.php");
+        $_SESSION['programID'] = $row['programID'];
+        echo($_SESSION);
+    }
+
 };
 
 ?>
