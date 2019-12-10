b5f0b70954befe1c6804ae153ea77d2b34ca1713
Eileen Santiago <eileenmvs@gmail.com>
2019-12-09 22:51:10 -0500

    update db connect

--- include/connect.php
+++ include/connect.php
@@ -2,14 +2,14 @@
 // Eileen's Sheridan Server db 
 // $dsn="mysql:host=localhost;dbname=santiaei_campus_eats;charset=utf8mb4";
 
+// $dbusername="santiaei_admin";
+// $dbpassword="GA47Y0;U}?kQ";
+
 // Use this db for your local host if your db name is 'CampusEats'
 $dsn="mysql:host=localhost;dbname=CampusEats;charset=utf8mb4";
 
 // use your db username and password 
-// $dbusername="CampusEats";
-// $dbpassword="GA47Y0;U}?kQ";
-
-$dbusername="santiaei_admin";
+$dbusername="CampusEats";
 $dbpassword="GA47Y0;U}?kQ";
 
 $pdo=new PDO($dsn, $dbusername,$dbpassword);
